d5d90c8a8085ce98ed7d8a9bc32f8c3d9fb77627
unknown <rmsmall7@gmail.com>
2019-05-10 15:59:16 -0400

    server is working

--- config/config.js
+++ config/config.js
@@ -3,26 +3,25 @@ module.exports =
 {
   "development": {
     "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
+    "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "port": 8889,
     "dialect": "mysql"
   },
   "test": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "port": 8889,
-    "dialect": "mysql"
+    "username": "root",
+    "password": "18@Glendale",
+    "database": "testdb",
+    "host": "localhost",
+    "dialect": "mysql",
+    
   },
   "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "port": 8889,
-    "dialect": "mysql"
+    "username": "root",
+    "password": "18@Glendale",
+    "database": "testdb",
+    "host": "localhost",
+    "dialect": "mysql",
+    
   }
 }
