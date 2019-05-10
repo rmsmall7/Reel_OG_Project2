83703a83f719dc344649cbf04b91dd7af9ffb306
Ismael Alexis Perez <46068538+iperez99@users.noreply.github.com>
2019-05-10 15:09:09 -0400

    Revert "Iperez branch"

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-var bodyParser = require("body-parser");
+
 var db = require("./models");
 
 var app = express();
@@ -22,19 +22,19 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
-// require("./routes/apiRoutes")(app);
+require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-// var syncOptions = { force: false };
+var syncOptions = { force: false };
 
-// // If running a test, set syncOptions.force to true
-// // clearing the `testdb`
-// if (process.env.NODE_ENV === "test") {
-//   syncOptions.force = true;
-// }
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync().then(function() {
+db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
